29b9692cc9b3800dde84415a989ac31066453b65
kimzoo <kimzoo5676@gmail.com>
2026-06-08 14:46:41 +0900

    feat: HTTPS 보안 설정 추가(proxy ssl header, ssl redirect, secure cookies)

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -5,3 +5,10 @@ DEBUG = False
 DATABASES = {
     "default": env.db(),
 }
+
+
+# --- HTTPS / behind Nginx proxy (Closes #12) ---
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
